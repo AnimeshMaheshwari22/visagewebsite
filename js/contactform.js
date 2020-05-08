d1903c19a1de047937409e0804b05c3cbcebf481
AnimeshMaheshwari22 <aniaero22@gmail.com>
2020-05-08 17:51:32 +0530

    Commit

---
+++ js/contactform.js
@@ -0,0 +1,38 @@
+
+  firebase.initializeApp(config);
+
+  var messagesRef = firebase.database().ref('messages');
+
+document.getElementById('contactForm').addEventListener('submit',submitForm);
+function submitForm(e){
+    e.preventDefault();
+
+    var name= getInputVal('name');
+    var nature= getSelection('nature');
+    var phone= getInputVal('phone');
+    var email= getInputVal('email');
+    var message= getInputVal('message');
+saveMessage(name,nature,phone,email,message);
+document.querySelector('.alert').style.display='block';
+
+setTimeout(function(){
+    document.querySelector('.alert').style.display='none';
+},3000);
+document.getElementById('contactForm').reset();
+}
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+function getSelection(id){
+    return document.getElementById(id).value;
+}
+function saveMessage(name,nature,phone,email,message){
+var newMessageRef=messagesRef.push();
+newMessageRef.set({
+name:name,
+nature:nature,
+phone:phone,
+email:email,
+message:message
+})
+}
